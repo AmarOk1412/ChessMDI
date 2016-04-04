0bbbfb40e44bba57e144a4171b4d4a06bbd2e2a9
AmarOk <blin.sebastien@enconn.fr>
2016-04-04 15:48:04 +0200

    initial commit

---
+++ src/jchess/core/Player.java
@@ -0,0 +1,116 @@
+/*
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Authors:
+ * Mateusz Sławomir Lach ( matlak, msl )
+ * Damian Marciniak
+ * Sébastien Blin <sebastien.blin@enconn.fr>
+ */
+package jchess.core;
+
+import java.io.Serializable;
+import java.security.SecureRandom;
+import java.math.BigInteger;
+
+
+/**
+ * Class representing the player in the game
+ */
+public class Player implements Serializable
+{
+	private String name;
+	private Colors color;
+    private playerTypes playerType; //TODO: Useful ?
+    public boolean goDown; //TODO: Move
+
+    public enum playerTypes //TODO: Useful ?
+    {
+        localUser, networkUser, computer
+    }
+    
+
+    /**
+     * Constructor for Player class
+     * @param color 
+     */
+    public Player(String color)
+    {
+    	SecureRandom random = new SecureRandom();
+        this.name = new BigInteger(130, random).toString(8) + "_" + color;
+        this.color = Colors.valueOf(color.toUpperCase());
+        this.goDown = false;
+    }
+
+    /**
+     * Constructor for Player class
+     * @param name
+     * @param color 
+     */
+    public Player(String name, String color)
+    {
+        this.name = name;
+        this.color = Colors.valueOf(color.toUpperCase());
+        this.goDown = false;
+    }
+
+    /** Method setting the players name
+     *  @param name name of player
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /** Method getting the players name
+     *  @return name of player
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /** Method setting the players type
+     *  @param type type of player - enumerate
+     */
+    public void setType(playerTypes type)
+    {
+        this.playerType = type;
+    }
+
+    /**
+     * @return the color
+     */
+    public Colors getColor()
+    {
+        return color;
+    }
+
+    /**
+     * @return the playerType
+     */
+    public playerTypes getPlayerType()
+    {
+        return playerType;
+    }
+
+    /**
+     * @return the goDown
+     */
+    public boolean isGoDown()
+    {
+        return goDown;
+    }    
+}
